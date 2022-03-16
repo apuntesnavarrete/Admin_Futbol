d90860804cc7b5dad2db31734d0e5a3a73a84fa7
apuntesnavarrete <apuntesnavarrete@gmail.com>
2022-03-16 01:49:41 -0600

    agregando variables

--- routes/resul/plibre.js
+++ routes/resul/plibre.js
@@ -6,7 +6,10 @@ const pool = require('../../database');
 router.get('/', async (req, res, next) => {
     const resul = await pool.query("SELECT * FROM `ed_jor_libre_a22` ORDER BY ID DESC LIMIT 30;");
     console.log(resul)
-    res.render('resul/resul', {resul});
+    let categoria = "Femenil"
+    let Liga = "Pro Champions League"
+    let logo_liga = "logochampions.png"
+    res.render('resul/resul', {resul,categoria,Liga,logo_liga});
 });
 
 
