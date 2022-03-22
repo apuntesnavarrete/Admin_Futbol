2ad319ebd66c39ef517177216fae56acad7c65ad
apuntesnavarrete <apuntesnavarrete@gmail.com>
2022-03-22 00:22:22 -0600

    Estable gitignore

--- conexion/keys.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    database: {
-       host     : 'database-1.cldforynkg5i.us-east-2.rds.amazonaws.com',
-       user     : 'admin',
-       password : 'secfe2414',
-       database : 'futbolce_zon58'
-   
-    }
-   };
