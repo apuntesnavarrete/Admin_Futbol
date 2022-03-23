2036ab8532d30a48e0cde5f9da7c61ece31de904
apuntesnavarrete <apuntesnavarrete@gmail.com>
2022-03-22 21:36:57 -0600

    Tablas generales a todas las categorias pro

--- routes/Tablageneral/pro.js
+++ routes/Tablageneral/pro.js
@@ -7,50 +7,29 @@ const pool = require('../../database');
 router.get('/libre', async(req, res, next)=> {
     const vistas = await pool.query("SELECT * FROM `PRO_general_libre_c21`");
     const categoria = 'Libre C2021'
-    const goleo = {}
-    const jornadas = await pool.query("SELECT * FROM `futbolce_zon58`.`ed_jor_libre_a20` ORDER BY `Jornada` DESC");
-    const Equipos = await pool.query("SELECT * FROM `futbolce_zon58`.`ed_jor_libre_a20` ORDER BY `Equipo` DESC , `Jornada` DESC ");
   
-    res.render('tablas/general.ejs',{vistas,categoria,goleo,jornadas,Equipos})
+    res.render('tablas/general.ejs',{vistas,categoria})
   });
 
 
-router.get('/femenil', async (req, res, next) => {
-    const resul = await pool.query("SELECT * FROM `pro_jor_femenil_A22` ORDER BY ID DESC LIMIT 30;");
-    console.log(resul)
-    let categoria = "femenil"
-    let Liga = "Pro Champions League"
-    let logo_liga = "logochampions.png"
-    let fondo = 'url("/images/fondochampions.jpg")';
-    let color = 'rgb(223 114 199)'
-    let jornada = "Semana"
+  router.get('/femenil', async(req, res, next)=> {
+    const vistas = await pool.query("SELECT * FROM `PRO_general_feme_a22`");
+    const categoria = 'Femenil A2022'
+  
+    res.render('tablas/general.ejs',{vistas,categoria})
+  });
+  router.get('/Sub21', async(req, res, next)=> {
+    const vistas = await pool.query("SELECT * FROM `pro_general_sub21_a22`");
+    const categoria = 'Sub-21 A2022'
+  
+    res.render('tablas/general.ejs',{vistas,categoria})
+  });
 
-    res.render('resul/resul', {resul,categoria,Liga,logo_liga,fondo,color,jornada});
-});
-router.get('/sub21', async (req, res, next) => {
-    const resul = await pool.query("SELECT * FROM `pro_jor_sub21_A22` ORDER BY ID DESC LIMIT 30;");
-    console.log(resul)
-    let categoria = "Sub-21"
-    let Liga = "Pro Champions League"
-    let logo_liga = "logochampions.png"
-    let fondo = 'url("/images/fondochampions.jpg")';
-    let color = 'rgb(233 202 107)'
-    let jornada = "Semana"
-
-    res.render('resul/resul', {resul,categoria,Liga,logo_liga,fondo,color,jornada});
-});
-
-router.get('/mixto', async (req, res, next) => {
-    const resul = await pool.query("SELECT * FROM `pro_jor_mix_A22` ORDER BY ID DESC LIMIT 30;");
-    console.log(resul)
-    let categoria = "Mixta"
-    let Liga = "Pro Champions League"
-    let logo_liga = "logochampions.png"
-    let fondo = 'url("/images/fondochampions.jpg")';
-    let color = 'rgb(213 189 255)'
-    let jornada = "Semana"
-
-    res.render('resul/resul', {resul,categoria,Liga,logo_liga,fondo,color,jornada});
-});
+  router.get('/mixta', async(req, res, next)=> {
+    const vistas = await pool.query("SELECT * FROM `Pro_general_mixto_a22`");
+    const categoria = 'Mixta A2022'
+  
+    res.render('tablas/general.ejs',{vistas,categoria})
+  });
 
 module.exports = router;
